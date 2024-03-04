d795743853d5e50ee826d9168771880dce6b80b5
zhu-to-u <2528487925@qq.com>
2024-03-04 17:55:28 +0800

    http基础

--- http-base/base3/gee/gee.go
+++ http-base/base3/gee/gee.go
@@ -1,1 +1,51 @@
 package gee
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+// HandlerFunc defines the request handler used by gee
+type HandlerFunc func(http.ResponseWriter, *http.Request) //请求处理函数
+
+// Engine implement the interface of ServeHTTP
+type Engine struct {
+	router map[string]HandlerFunc //是一个字符串到HandlerFunc函数类型的映射
+}
+
+// New is the constructor of gee.Engine
+func New() *Engine {
+	return &Engine{router: make(map[string]HandlerFunc)} //创建并返回一个新的Engine的实例
+}
+
+// 私有方法（小写）
+func (engine *Engine) addRoute(method string, pattern string, handler HandlerFunc) {
+	key := method + "-" + pattern
+	log.Printf("Route %4s - %s", method, pattern)
+	engine.router[key] = handler
+}
+
+// GET defines the method to add GET request
+func (engine *Engine) GET(pattern string, handler HandlerFunc) {
+	engine.addRoute("GET", pattern, handler)
+}
+
+// POST defines the method to add POST request
+func (engine *Engine) POST(pattern string, handler HandlerFunc) {
+	engine.addRoute("POST", pattern, handler)
+}
+
+// Run defines the method to start a http server
+func (engine *Engine) Run(addr string) (err error) {
+	return http.ListenAndServe(addr, engine)
+}
+
+func (engine *Engine) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	key := req.Method + "-" + req.URL.Path
+	if handler, ok := engine.router[key]; ok {
+		handler(w, req)
+	} else {
+		fmt.Fprintf(w, "404 NOT FOUND: %s\n", req.URL)
+	}
+}
--- http-base/base3/main.go
+++ http-base/base3/main.go
@@ -1,1 +1,24 @@
-package base3
+package main
+
+import (
+	"fmt"
+	"gee"
+	"net/http"
+)
+
+func main() {
+	r := gee.New()
+	r.GET("/", func(w http.ResponseWriter, req *http.Request) { //第二个参数是一个匿名函数
+		fmt.Fprintf(w, "URL.Path=%q\n", req.URL.Path) //返回请求给客户端
+	})
+
+	r.GET("/hello", func(w http.ResponseWriter, req *http.Request) {
+		for k, v := range req.Header {
+			fmt.Fprintf(w, "Header[%q]=%q\n", k, v)
+		}
+	})
+
+	r.Run(":9999")
+}
+
+//没有启动服务器之前，前面的注册路由和处理函数会正常执行，但不会立即出发处理函数，处理函数会在客户端发起请求并且服务器正常运行时才会被调用
